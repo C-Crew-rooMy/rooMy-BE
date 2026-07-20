51324f96bef41efe3c45e3442f1a1f005bffdf58
apple <apple@apples-MacBook-Air.local>
2026-07-20 19:42:13 +0900

    fix: 경로수정

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,6 @@
 import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { Visibility } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PostService {
