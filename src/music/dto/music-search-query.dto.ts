74d2cc6b5f0fb7e11bc1cf34783efde7470d4dd6
hogu-giriboy <choo6078@kakao.com>
2026-09-11 01:41:12 +0900

    test: E2E 시나리오 검증

--- src/music/dto/music-search-query.dto.ts
+++ src/music/dto/music-search-query.dto.ts
@@ -3,6 +3,6 @@ import { IsNotEmpty, IsString, Matches } from 'class-validator';
 export class MusicSearchQueryDto {
   @IsString({ message: '검색어는 문자열이어야 합니다.' })
   @IsNotEmpty({ message: '검색어를 입력해주세요.' })
-  @Matches(/\\S/, { message: '검색어를 입력해주세요.' })
+  @Matches(/\S/, { message: '검색어를 입력해주세요.' })
   q: string;
 }
