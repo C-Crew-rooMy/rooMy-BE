012589f16666901e3b715d231dc79209b67f2c84
hogu-giriboy <choo6078@kakao.com>
2026-09-11 01:27:12 +0900

    feat: iTunes 외부 API 호출 서비스 구현

---
+++ src/music/music.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { ItunesService } from './itunes.service';
+
+@Module({
+  providers: [ItunesService],
+  exports: [ItunesService],
+})
+export class MusicModule {}
